d6b06a671d3a7aac1520b9e5831323774fb5af1b
dearaujofelipe <mgdefelipe@outlook.com>
2024-08-15 19:12:40 -0300

    addItemForm

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,10 +1,37 @@
+import { useRef, useState } from 'react';
 import Button from './Button';
 
-export default function AddItemForm() {
+export default function AddItemForm({ setItems }) {
+  const [itemText, setItemText] = useState('');
+  const inputRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!itemText) {
+      alert('cant at empty task');
+      inputRef.current.focus();
+    }
+
+    const newItem = {
+      id: new Date().getTime(),
+      name: itemText,
+      packed: false,
+    };
+
+    setItems((prev) => [...prev, newItem]);
+    setItemText('');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
-      <input type="text" />
+      <input
+        ref={inputRef}
+        value={itemText}
+        onChange={(e) => setItemText(e.target.value)}
+        autoFocus={true}
+      />
       <Button>Add to list</Button>
     </form>
   );
