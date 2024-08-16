00c99e72be0bb0f007b149fedfc37e1f034ce132
dearaujofelipe <mgdefelipe@outlook.com>
2024-08-16 19:35:41 -0300

    finish secondary buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,39 @@ import { initialItems } from '../lib/constants';
 function App() {
   const [items, setItems] = useState(initialItems);
 
+  const handleAddItem = (newItemText) => {
+    const newItem = {
+      id: new Date().getTime(),
+      name: newItemText,
+      packed: false,
+    };
+
+    const newItems = [...items, newItem];
+    setItems(newItems);
+  };
+
+  const handleRemoveAllItems = () => {
+    setItems([]);
+  };
+
+  const handleResetToInitial = () => {
+    setItems(initialItems);
+  };
+
+  const handleMarkAllAsComplete = () => {
+    const newItems = items.map((item) => {
+      return { ...item, packed: true };
+    });
+    setItems(newItems);
+  };
+
+  const handleMarkAllAsIncomplete = () => {
+    const newItems = items.map((item) => {
+      return { ...item, packed: false };
+    });
+    setItems(newItems);
+  };
+
   return (
     <>
       <BackgroundHeading />
@@ -16,7 +49,13 @@ function App() {
       <main>
         <Header />
         <ItemList items={items} />
-        <Sidebar setItems={setItems} />
+        <Sidebar
+          handleAddItem={handleAddItem}
+          handleRemoveAllItems={handleRemoveAllItems}
+          handleResetToInitial={handleResetToInitial}
+          handleMarkAllAsComplete={handleMarkAllAsComplete}
+          handleMarkAllAsIncomplete={handleMarkAllAsIncomplete}
+        />
       </main>
 
       <Footer />
--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,16 +1,25 @@
-import { secondaryButtons } from '../lib/constants';
 import Button from './Button';
 
-export default function ButtonGroup() {
+export default function ButtonGroup({
+  handleRemoveAllItems,
+  handleResetToInitial,
+  handleMarkAllAsComplete,
+  handleMarkAllAsIncomplete,
+}) {
   return (
     <section className="button-group">
-      {secondaryButtons.map((text) => {
-        return (
-          <Button key={text} type="secondary">
-            {text}
-          </Button>
-        );
-      })}
+      <Button onClick={handleMarkAllAsComplete} buttontype="secondary">
+        Mark all as complete
+      </Button>
+      <Button onClick={handleMarkAllAsIncomplete} buttontype="secondary">
+        Mark all as incomplete
+      </Button>
+      <Button onClick={handleResetToInitial} buttontype="secondary">
+        Reset to initial
+      </Button>
+      <Button onClick={handleRemoveAllItems} buttontype="secondary">
+        Remove all items
+      </Button>
     </section>
   );
 }
