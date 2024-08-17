b8e4edeca95da4c4e50dcc2d383e0ca30180db5f
dearaujofelipe <mgdefelipe@outlook.com>
2024-08-17 18:33:24 -0300

    emptyView, number of items packed and total number of items functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -62,7 +62,10 @@ function App() {
       <BackgroundHeading />
 
       <main>
-        <Header />
+        <Header
+          numberOfItemsPacked={items.filter((item) => item.packed).length}
+          totalNumberOfItems={items.length}
+        />
         <ItemList
           items={items}
           handleDeleteItem={handleDeleteItem}
